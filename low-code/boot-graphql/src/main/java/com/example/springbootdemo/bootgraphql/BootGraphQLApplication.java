330cc92c3d898a70560530788ede1378105212e3
lee <crossthebackstreet@gmail.com>
2021-07-14 09:53:01 +0800

    add low code module

---
+++ low-code/boot-graphql/src/main/java/com/example/springbootdemo/bootgraphql/BootGraphQLApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.bootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGraphQLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootGraphQLApplication.class, args);
+    }
+
+}

00db46b40ea8a598e5a7ebca6c2d6ff6176d4f1b
lee <crossthebackstreet@gmail.com>
2021-07-14 09:45:28 +0800

    add web security module

---
+++ web-security/servlet-security/src/main/java/com/example/springbootdemo/servletsecurity/ServletSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.servletsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletSecurityApplication.class, args);
+    }
+
+}

14883a7e51e45d8d77763215f49f92296cd7fe78
lee <crossthebackstreet@gmail.com>
2021-07-14 09:44:42 +0800

    add monitor module

---
+++ monitor/prometheus/src/main/java/com/example/springbootdemo/prometheus/PrometheusMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusMonitorApplication.class, args);
+    }
+
+}

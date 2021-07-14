e3e42d6028462877bc9e56e738c9382115fe426a
lee <crossthebackstreet@gmail.com>
2021-07-14 09:42:35 +0800

    add start module

---
+++ start/src/main/java/com/example/springbootdemo/start/StartApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+
+    @SpringBootApplication
+    public static class Boot {}
+
+}
